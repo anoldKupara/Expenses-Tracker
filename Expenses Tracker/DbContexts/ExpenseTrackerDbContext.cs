af49b542121077b23319920e7482daf474b04fd5
Anold Kupara <kuparaanold20@gmail.com>
2023-01-23 14:32:36 +0200

    added the ExpenseTrackerDbContext class

---
+++ Expenses Tracker/DbContexts/ExpenseTrackerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expenses_Tracker.DbContexts
+{
+    public class ExpenseTrackerDbContext : DbContext
+    {
+        public DbSet<Budget> Budgets { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=ExpenseTracker;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
