fbd8fef2baf58b1bd5e45f4b37a74b53c3314cf2
Anold Kupara <kuparaanold20@gmail.com>
2023-01-23 14:40:15 +0200

    added the Category model class

---
+++ Expenses Tracker/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Expenses_Tracker.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Budget> Budgets { get; set; }
+        public ICollection<Expense> Expenses { get; set; }
+    }
+}
--- Expenses Tracker/Models/Expense.cs
+++ Expenses Tracker/Models/Expense.cs
@@ -1,6 +1,12 @@
 ﻿namespace Expenses_Tracker.Models
 {
-    public class Expenses
+    public class Expense
     {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
